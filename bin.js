665e00b034a59ae46e3225a49a000529e491f4e7
xcatliu <xcatliu@gmail.com>
2023-05-25 15:14:07 +0800

    feat: 调整 bin 的执行参数

--- bin.js
+++ bin.js
@@ -5,4 +5,7 @@ const { spawn } = require('child_process');
 // 获取命令行参数
 const args = process.argv.slice(2);
 
-spawn('npm', ['start', ...args], { stdio: 'inherit' });
+spawn('npm', ['start', ...args], {
+  stdio: 'inherit',
+  cwd: __dirname,
+});
